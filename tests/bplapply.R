c2c52ed0286a02623230f15261d5b8a880d0d335
hb <hb@aroma-project.org>
2016-05-15 16:52:55 -0700

    TESTS: Now testing using different types of futures

--- tests/bplapply.R
+++ tests/bplapply.R
@@ -1,19 +1,30 @@
 library("BiocParallel.FutureParam")
+oopts <- options(mc.cores=2L, warn=1L)
+strategies <- future:::supportedStrategies()
+strategies <- setdiff(strategies, "multiprocess")
+
 register(FutureParam())
 plan(lazy)
 
 message("*** bplapply() w/ FutureParam ...")
 
-message("- sqrt()")
-x <- 1:10
-expected <- lapply(x, FUN=sqrt)
-current <- bplapply(x, FUN=sqrt)
-stopifnot(identical(expected, current))
+for (strategy in strategies) {
+  message(sprintf("- plan('%s') ...", strategy))
+  plan(strategy)
 
-message("- identity()")
-current <- bplapply(list(), FUN=identity)
-stopifnot(identical(list(), current))
+  message("  - sqrt()")
+  x <- 1:10
+  expected <- lapply(x, FUN=sqrt)
+  current <- bplapply(x, FUN=sqrt)
+  stopifnot(identical(expected, current))
+
+  message("  - identity()")
+  current <- bplapply(list(), FUN=identity)
+  stopifnot(identical(list(), current))
+
+  message(sprintf("- plan('%s') ... DONE", strategy))
+} ## for (strategy ...)
 
 message("*** bplapply() w/ FutureParam ... DONE")
 
-
+options(oopts)
--- tests/bpmapply.R
+++ tests/bpmapply.R
@@ -1,28 +1,42 @@
 library("BiocParallel.FutureParam")
+oopts <- options(mc.cores=2L, warn=1L)
+strategies <- future:::supportedStrategies()
+strategies <- setdiff(strategies, "multiprocess")
+
 register(FutureParam())
 plan(lazy)
 
 message("*** bpmapply() w/ FutureParam ...")
 
-message("- rep()")
-a <- 1:4
-b <- 4:1
-expected <- mapply(rep, a, b)
-current <- bpmapply(rep, a, b)
-stopifnot(identical(expected, current))
+for (strategy in strategies) {
+  message(sprintf("- plan('%s') ...", strategy))
+  plan(strategy)
 
-message("- rep() w/ named arguments")
-a <- 1:4
-b <- 4:1
-expected <- mapply(rep, times=a, x=b)
-current <- bpmapply(rep, times=a, x=b)
-stopifnot(identical(expected, current))
+  message("  - rep()")
+  a <- 1:4
+  b <- 4:1
+  expected <- mapply(rep, a, b)
+  current <- bpmapply(rep, a, b)
+  stopifnot(identical(expected, current))
 
-message("- rep() w/ named arguments + MoreArgs")
-a <- 1:4
-b <- 4:1
-expected <- mapply(rep, times=a, MoreArgs = list(x=42))
-current <- bpmapply(rep, times=a, MoreArgs = list(x=42))
-stopifnot(identical(expected, current))
+  message("  - rep() w/ named arguments")
+  a <- 1:4
+  b <- 4:1
+  expected <- mapply(rep, times=a, x=b)
+  current <- bpmapply(rep, times=a, x=b)
+  stopifnot(identical(expected, current))
+
+  message("  - rep() w/ named arguments + MoreArgs")
+  a <- 1:4
+  b <- 4:1
+  expected <- mapply(rep, times=a, MoreArgs = list(x=42))
+  current <- bpmapply(rep, times=a, MoreArgs = list(x=42))
+  stopifnot(identical(expected, current))
+
+  message(sprintf("- plan('%s') ... DONE", strategy))
+} ## for (strategy ...)
 
 message("*** bpmapply() w/ FutureParam ... DONE")
+
+options(oopts)
+
