5b8fd78d3ca3f7dae6e2d78774986758dfdcbc26
jasenfinch <jsf9@aber.ac.uk>
2022-04-30 10:16:34 +0100

    add missing function import. Update NAMESPACE

--- R/FutureParam-class.R
+++ R/FutureParam-class.R
@@ -179,7 +179,7 @@ setMethod("bplapply", c("ANY", "FutureParam"), function(X, FUN, ..., BPREDO=list
 
 
 #' @importFrom methods setMethod
-#' @importFrom BiocParallel bploop bplog bptimeout bpstopOnError bpoptions
+#' @importFrom BiocParallel bploop bplog bptimeout bpstopOnError bpoptions bpRNGseed
 #' @importFrom future future resolve value
 setMethod("bpiterate", c("ANY", "ANY", "FutureParam"), function(ITER, FUN, ..., REDUCE, init, reduce.in.order=FALSE, BPREDO = list(),BPPARAM=bpparam(),BPOPTIONS = bpoptions()) {
   .composeTry <- importBP(".composeTry")
